6f74d1fc753c35a85c2514907c129c360ed49549
Dhananjaya Bulathsinghala <dlakmalb@gmail.com>
2025-12-11 13:52:00 +0530

    Add Project & Task Entities with Enum Support

---
+++ backend/src/Entity/Task.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace App\Entity;
+
+use App\Enum\TaskPriority;
+use App\Enum\TaskStatus;
+use App\Repository\TaskRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TaskRepository::class)]
+class Task
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\ManyToOne(inversedBy: 'tasks')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Project $project = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $description = null;
+
+    #[ORM\Column(length: 20)]
+    private ?string $status = null;
+
+    #[ORM\Column(length: 20)]
+    private ?string $priority = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $createdAt = null;
+
+    public function __construct()
+    {
+        $this->createdAt = new \DateTimeImmutable();
+
+        $this->status = TaskStatus::TODO->value;
+        $this->priority = TaskPriority::MEDIUM->value;
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getProject(): ?Project
+    {
+        return $this->project;
+    }
+
+    public function setProject(?Project $project): static
+    {
+        $this->project = $project;
+
+        return $this;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getStatus(): TaskStatus
+    {
+        return TaskStatus::from($this->status);
+    }
+
+    public function setStatus(TaskStatus $status): self
+    {
+        $this->status = $status->value;
+
+        return $this;
+    }
+
+    public function getPriority(): TaskPriority
+    {
+        return TaskPriority::from($this->priority);
+    }
+
+    public function setPriority(TaskPriority $priority): self
+    {
+        $this->priority = $priority->value;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): static
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+}
